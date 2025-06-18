f33481b360026008ac9a65c3a351b78f3c551f83
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-06-18 22:21:49 +0400

    package

---
+++ src/minibook/__init__.py
@@ -0,0 +1,8 @@
+"""
+MiniBook - A tool to create a minibook from a list of links.
+Supports both MkDocs and Jinja2/HTML generation.
+"""
+
+from .main import generate_html, generate_mkdocs_project, main
+
+__all__ = ['generate_html', 'generate_mkdocs_project', 'main']
