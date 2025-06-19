21617ae5253b0859bd141d26392375afa8f34389
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-06-19 07:05:41 +0400

    testing tuples

---
+++ src/tests/test_tuples.py
@@ -0,0 +1,27 @@
+def test_tuple_unpacking():
+    a, b = (1, 2)
+    assert a == 1
+    assert b == 2
+    assert type(a) == int
+    assert type(b) == int
+
+def test_tuple_unpacking_strings():
+    links = "1;2"
+    a, b = links.split(";")
+    assert a == "1"
+    assert b == "2"
+    assert type(a) == str
+    assert type(b) == str
+
+def test_tuple_unpacking_strings_with_spaces():
+    links = "1;2,3;4"
+    a,b = links.split(",")
+    assert a == "1;2"
+    assert b == "3;4"
+    assert type(a) == str
+    assert type(b) == str
+
+def test_tuple_no_comma():
+    links = "1;2"
+    pairs = links.split(",")
+    assert pairs[0] == "1;2"
