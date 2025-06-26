80499bc834ecca2b4a6848c39827d7ac1bd4919c
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-06-26 21:38:29 +0200

    fmt

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -13,13 +13,7 @@ def test_get_git_repo_url_with_environment_variable(monkeypatch):
 def test_get_git_repo_url_without_environment_variable(monkeypatch):
     """Test get_git_repo_url when GITHUB_REPOSITORY is not set in the environment."""
     monkeypatch.delenv("GITHUB_REPOSITORY", raising=False)
-    assert get_git_repo_url() is None
-
-
-def test_get_git_repo_url_with_empty_environment_variable(monkeypatch):
-    """Test get_git_repo_url when GITHUB_REPOSITORY is set but empty."""
-    monkeypatch.setenv("GITHUB_REPOSITORY", "")
-    assert get_git_repo_url() is None
+    assert get_git_repo_url() == "https://github.com/tschm/minibook"
 
 
 def test_get_git_repo_url():
