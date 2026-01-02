cd71e2bbd8b5008578d87cf9a8fc99067c310304
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2026-01-02 14:39:42 +0400

    Remove versioning from __init__.py
    Removed version retrieval from importlib.metadata.

--- src/minibook/__init__.py
+++ src/minibook/__init__.py
@@ -3,10 +3,6 @@
 Generates a clean, responsive HTML webpage using Jinja2 templates.
 """
 
-import importlib.metadata
-
 from .main import entrypoint, generate_html
-
-__version__ = importlib.metadata.version("minibook")
 
 __all__ = ["entrypoint", "generate_html"]
